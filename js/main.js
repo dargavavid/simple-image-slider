e116b07f2e4800ff5a7ba6852bd8a62e5775aaf8
dargavavid <dargavavid@gmail.com>
2017-12-14 00:33:18 +0100

    Bind right arrow

--- js/main.js
+++ js/main.js
@@ -3,36 +3,45 @@ let arrowLeft = document.querySelector("#arrow-left");
 let arrowRight = document.querySelector("#arrow-right");
 let current = 0;
 
-//To hide all images.
+//Hide all images.
 function reset() {
     sliderImages.forEach(el => el.style.display = "none");
 }
 
-// To initialize slider
+//Initialize slider
 function startSlide() {
     reset();
     sliderImages[0].style.display = "block";
 }
 
-// Show prev
+//Show prev
 function slideLeft() {
     reset();
     sliderImages[current - 1].style.display = "block";
     current -= 1; 
 }
 
-// Show next
+//Show next
 function slideRight() {
     reset();
     sliderImages[current + 1].style.display = "block";
     current++;
 }
 
+//Left arrow click
 arrowLeft.addEventListener("click", function () {
     if (current === 0) {
         current = sliderImages.length;
     }
     slideLeft();
+}, false);
+
+//Right arrow click
+arrowRight.addEventListener("click", function () {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
 }, false);
 
 startSlide();
