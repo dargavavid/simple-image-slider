9781864e32ab56ef8c021acc9c75f25198cd7a40
dargavavid <dargavavid@gmail.com>
2017-12-14 00:12:34 +0100

    Declare global variables

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let sliderImages = document.querySelectorAll(".slide");
+let arrowLeft = document.querySelector("#arrow-left");
+let arrowRight = document.querySelector("#arrow-right");
+let current = 0;
