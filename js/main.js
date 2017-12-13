518c5d8d4c0e45c112678790dd90d72c812a4069
dargavavid <dargavavid@gmail.com>
2017-12-14 00:16:59 +0100

    Create reset function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ let sliderImages = document.querySelectorAll(".slide");
 let arrowLeft = document.querySelector("#arrow-left");
 let arrowRight = document.querySelector("#arrow-right");
 let current = 0;
+
+//To hide all images.
+function reset() {
+    sliderImages.forEach(el => el.style.display = "none");
+}
